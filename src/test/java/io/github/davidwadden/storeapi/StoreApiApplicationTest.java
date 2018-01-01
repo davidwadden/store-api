e5e997ff74353d44e526c7e15cfb7db0370fb2a1
David Wadden <dwadden@pivotal.io>
2018-01-01 12:57:46 -0600

    Adds dependency for JUnit 5.0.2
      + Applies JUnit Gradle plugin

---
+++ src/test/java/io/github/davidwadden/storeapi/StoreApiApplicationTest.java
@@ -0,0 +1,18 @@
+package io.github.davidwadden.storeapi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class StoreApiApplicationTest {
+
+	@DisplayName("loads a context")
+	@Test
+    void contextLoads() {
+	}
+
+}
